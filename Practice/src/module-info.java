1b38e57299fcabc8b0155d95104055b82cf7fcac
geruman27 <cksah501@gmail.com>
2023-02-19 00:16:44 +0900

    연습연습

---
+++ Practice/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dnlse
+ *
+ */
+module Practice {
+}
---
+++ Practice/src/num1Practice/_01_Practice.java
@@ -0,0 +1,11 @@
+package num1Practice;
+
+public class _01_Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		var str = "Hello java";
+	}
+
+}
