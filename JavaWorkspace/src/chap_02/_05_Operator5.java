9ec612f27e309ec5c5332bef23ba7f10c9e04e08
geruman27 <cksah501@gmail.com>
2023-02-19 21:52:17 +0900

    2023-02-19

---
+++ JavaWorkspace/src/chap_02/_05_Operator5.java
@@ -0,0 +1,25 @@
+package chap_02;
+
+public class _05_Operator5 {
+    public static void main(String[] args) {
+        // 삼항 연산자
+        //결과 = (조건) ? (참의 경우 결과 값) : (거짓일 경우 결과값)
+        int x = 5;
+        int y = 3;
+        int max = (x > y) ? x : y;
+        System.out.println(max);
+
+        int min = (x < y) ? x : y;
+        System.out.println(min);
+
+        x = 3;
+        y = 3;
+        boolean b = (x == y) ? true : false;
+        System.out.println(b);
+
+        String s = (x != y) ? "달라요" : "같아요";
+        System.out.println(s);
+
+
+    }
+}
---
+++ JavaWorkspace/src/chap_02/_05_Quiz.java
@@ -0,0 +1,11 @@
+package chap_02;
+
+public class _05_Quiz {
+    public static void main(String[] args) {
+        int i = 125;
+
+
+        String result = (i >= 120) ? "탑승 가능합니다." : "탑승 불가능합니다";
+        System.out.println("키가 " + i + "cm 이므로 " + result  );
+    }
+}
