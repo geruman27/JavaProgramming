2d261e46a6a7f5e6db4104e071990cc855de36a6
geruman27 <cksah501@gmail.com>
2023-02-20 21:06:19 +0900

    2023-02-20 복습

--- Practice/src/num1Practice/_01_Practice.java
+++ Practice/src/num1Practice/_01_Practice.java
@@ -4,8 +4,12 @@ public class _01_Practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i;
+		i = 10;
 		
-		var str = "Hello java";
-	}
+		double d = 20.0;
+		System.out.println(d);
+
+	} 
 
 }
